b7a85818733454aff9c9a3efcbbf0c5ad0557bd3
Christophe Willemsen <willemsen.christophe@gmail.com>
2017-04-09 19:21:38 +0400

    quick fix for autoloading proxy entities

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -6,4 +6,27 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
 {
+    private $autoloader;
+
+    public function boot()
+    {
+        $container = &$this->container;
+
+        $this->autoloader = function($class) use (&$container) {
+            if (0 === strpos($class, 'neo4j_ogm_proxy')) {
+                $cacheDir = $container->getParameter('kernel.cache_dir') . DIRECTORY_SEPARATOR . 'neo4j';
+                $file = $cacheDir.DIRECTORY_SEPARATOR.$class.'.php';
+                if (file_exists($file)) {
+                    require_once $file;
+                }
+            }
+        };
+
+        spl_autoload_register($this->autoloader);
+    }
+
+    public function shutdown()
+    {
+        spl_autoload_unregister($this->autoloader);
+    }
 }
